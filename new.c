ae0324e2971827f71afda017fe706baef477d0ac
Eliza Ahmed <elizarafee@gmail.com>
2020-02-13 16:07:42 +0600

    Flow chart: Switch case executed

--- new.c
+++ new.c
@@ -48,8 +48,32 @@ int main()
      }
     printf("Break/Continue no: %d\n", i);
    }
-   
-   
+    
+    char op;
+    int k, l;
+    printf("enter an operator from * / + / - : \n");
+    scanf("%c", &op);
+    printf("enter two digits: ");
+    scanf("%d %d", &k, &l);
+    
+    switch (op)
+    {
+      case '*':
+      printf("%d * %d = %d", k, l, k*l);
+      break;
+
+      case '-':
+      printf("%d - %d = %d", k, l, k-l);
+      break;
+
+      case '+':
+      printf("%d - %d = %d", k, l, k+l);
+      break;
+
+      default:
+      printf("Error! operator is not correct");
+      break;    
+    }
    
     return 0;
 }
